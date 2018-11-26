520ea680edde768ec4675bf0e0ca89e9d25d789b
lxlxw <19168066@qq.com>
2018-11-26 19:26:06 +0800

    update code

--- README.md
+++ README.md
@@ -1,34 +1,17 @@
 # wps store micro service
 
-
-# CA TLS
-cd conf/certs
-openssl genrsa -out server.key 2048
-openssl ecparam -genkey -name secp384r1 -out server.key
-openssl req -new -x509 -sha256 -key server.key -out server.pem -days 3650
-
-Country Name (2 letter code) [XX]:
-State or Province Name (full name) []:
-Locality Name (eg, city) [Default City]:
-Organization Name (eg, company) [Default Company Ltd]:
-Organizational Unit Name (eg, section) []:
-Common Name (eg, your name or your server's hostname) []:wps grpc micro service
-Email Address []:
-
 # protoc-gen
 mv protoc-gen-swagger /usr/local/go/bin
 mv protoc-gen-grpc-gateway /usr/local/go/bin
-
 
 # 编译google.api
 protoc -I . --go_out=plugins=grpc,Mgoogle/protobuf/descriptor.proto=github.com/golang/protobuf/protoc-gen-go/descriptor:. google/api/*.proto
 
 #编译rpc.proto为rpc.pb.proto
-protoc -I . --go_out=plugins=grpc,Mgoogle/api/annotations.proto=wps_store/proto/google/api:. ./rpc.proto
+protoc -I/usr/local/include -I. \-I$GOPATH/src \-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \--go_out=plugins=grpc,Mgoogle/api/annotations.proto=wps_store/rpc/google/api:. \rpc.proto
 
 #编译rpc.proto为rpc.pb.gw.proto
-protoc --grpc-gateway_out=logtostderr=true:. ./rpc.proto
+protoc -I/usr/local/include -I. \-I$GOPATH/src \-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \--grpc-gateway_out=logtostderr=true:. \rpc.proto
 
-# TODO：
-# 1.TLS认证
-# 2.JWT TOKEN
+#编译 swagger.json
+protoc -I/usr/local/include -I. \-I$GOPATH/src  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \--swagger_out=logtostderr=true:. \rpc.proto
--- service/object.go
+++ service/object.go
@@ -1,46 +1,82 @@
 package service
 
 import (
-	"log"
-	//"bytes"
-
-	//"github.com/ks3sdklib/aws-sdk-go/aws"
 	"github.com/ks3sdklib/aws-sdk-go/aws/awserr"
-	//"github.com/ks3sdklib/aws-sdk-go/service/s3"
 
 	ws3 "wps_store/pkg/s3"
 
-	pb "wps_store/proto"
+	pb "wps_store/rpc"
 )
 
 /*
 * 上传文件
 *
  */
-func PutObject(Name string) pb.PutObjectResponse {
+func PutObject(r *pb.PutObjectRequest) pb.PutObjectResponse {
+	client, _ := ws3.New(r.Store)
 
-	var result pb.PutObjectResponse
+	//TODO: 要处理下key， 拆分. 然后将keyname做成文件目录
 
-	//判断是ks3还是s3，去配置文件找出相应配置
-	//New封装
-	//params传入
-	//如果带id，则先查找下该文件是否还存在，如果存在，直接返回外链
-
-	client, err := ws3.New()
-	if err != nil {
+	//判断文件是否存在
+	objectExist := client.HeadObject(r.Bucketname, r.Key)
+	if objectExist == true {
+		url, err := client.GetObjectPresignedUrl(r.Bucketname, r.Key, r.Expiretime)
+		if err == nil {
+			return pb.PutObjectResponse{Msg: "success", Code: 200, Data: url}
+		}
 	}
 
-	err1 := client.PutObject()
-	if reqErr, ok := err1.(awserr.RequestFailure); ok {
+	err := client.PutObject(r.Bucketname, r.Key, r.Filecontent, r.Contenttype, r.Publicread, r.Contentmaxlength, r.Expiretime)
+	if reqErr, ok := err.(awserr.RequestFailure); ok {
 		return pb.PutObjectResponse{Msg: err.(awserr.Error).Code(), Code: int32(reqErr.StatusCode())}
 	}
-
-	log.Println("sss")
-	
-	result = pb.PutObjectResponse{
-		Msg:  "success",
-		Code: 200,
-		Data: Name,
+	url, err := client.GetObjectPresignedUrl(r.Bucketname, r.Key, r.Expiretime)
+	if err != nil {
+		if reqErr, ok := err.(awserr.RequestFailure); ok {
+			return pb.PutObjectResponse{Msg: err.(awserr.Error).Code(), Code: int32(reqErr.StatusCode())}
+		}
 	}
-	return result
+	return pb.PutObjectResponse{Msg: "success", Code: 200, Data: url}
+}
+
+/*
+*  下载文件
+*
+ */
+func GetObject(r *pb.GetObjectRequest) pb.GetObjectResponse {
+	client, _ := ws3.New(r.Store)
+
+	resBody, err := client.GetObject(r.Bucketname, r.Key, r.Contenttype)
+	if reqErr, ok := err.(awserr.RequestFailure); ok {
+		return pb.GetObjectResponse{Msg: err.(awserr.Error).Code(), Code: int32(reqErr.StatusCode())}
+	}
+	return pb.GetObjectResponse{Msg: "success", Code: 200, Data: resBody}
+}
+
+/*
+*  获取文件下载外链
+*
+ */
+func GetObjectPresignedUrl(r *pb.GetObjectPresignedUrlRequest) pb.GetObjectPresignedUrlResponse {
+	client, _ := ws3.New(r.Store)
+
+	url, err := client.GetObjectPresignedUrl(r.Bucketname, r.Key, r.Expiretime)
+	if reqErr, ok := err.(awserr.RequestFailure); ok {
+		return pb.GetObjectPresignedUrlResponse{Msg: err.(awserr.Error).Code(), Code: int32(reqErr.StatusCode())}
+	}
+	return pb.GetObjectPresignedUrlResponse{Msg: "success", Code: 200, Data: url}
+}
+
+/*
+*  生成文件上传外链
+*
+ */
+func PutObjectPresignedUrl(r *pb.PutObjectPresignedUrlRequest) pb.PutObjectPresignedUrlResponse {
+	client, _ := ws3.New(r.Store)
+
+	presignedUrl, err := client.PutObjectPresignedUrl(r.Bucketname, r.Key, r.Contenttype, r.Publicread, r.Contentmaxlength, r.Expiretime)
+	if reqErr, ok := err.(awserr.RequestFailure); ok {
+		return pb.PutObjectPresignedUrlResponse{Msg: err.(awserr.Error).Code(), Code: int32(reqErr.StatusCode())}
+	}
+	return pb.PutObjectPresignedUrlResponse{Msg: "success", Code: 200, Data: presignedUrl}
 }
