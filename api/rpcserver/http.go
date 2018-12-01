2084b0d69818e0ff213a8e7332f39882ad78ae02
linxiaowu <19168066@qq.com>
2018-12-01 20:37:18 +0800

    feat(http): add swagger api handlefunc

--- api/rpcserver/http.go
+++ api/rpcserver/http.go
@@ -3,11 +3,15 @@ package rpcserver
 import (
 	"log"
 	"net/http"
+	"strings"
+	"path"
 
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
+	"github.com/elazarl/go-bindata-assetfs"
+	"wps_store/pkg/swagger"
 
 	gw "wps_store/rpc"
 )
@@ -24,16 +28,57 @@ func RunHttpServer() (err error) {
 
 	// grpc服务地址
 	EndPoint = ":" + ServerPort
-	mux := runtime.NewServeMux()
+	gwmux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithInsecure()}
 
 	// HTTP转grpc
-	err = gw.RegisterStoreApiServiceHandlerFromEndpoint(ctx, mux, EndPoint, opts)
+	err = gw.RegisterStoreApiServiceHandlerFromEndpoint(ctx, gwmux, EndPoint, opts)
 	if err != nil {
 		grpclog.Fatalf("Register handler err:%v\n", err)
 	}
+
+
 	HttpEndPoint = ":" + ServerHttpPort
+	
+	// swagger ui
+	mux := http.NewServeMux()
+    mux.Handle("/", gwmux)
+    mux.HandleFunc("/swagger/", serveSwaggerFile)
+	serveSwaggerUI(mux)
+	
 	log.Println("HTTP Listen success:", HttpEndPoint)
-	http.ListenAndServe(HttpEndPoint, mux)
+
+    s := &http.Server{
+        Addr:      HttpEndPoint,
+        Handler:   http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+            mux.ServeHTTP(w, r)
+        }),
+    }
+	s.ListenAndServe()
 	return err
+}
+
+func serveSwaggerFile(w http.ResponseWriter, r *http.Request) {
+	if ! strings.HasSuffix(r.URL.Path, "swagger.json") {
+	  log.Printf("Not Found: %s", r.URL.Path)
+	  http.NotFound(w, r)
+	  return
+  }
+
+  p := strings.TrimPrefix(r.URL.Path, "/swagger/")
+  p = path.Join("rpc", p)
+
+  log.Printf("Serving swagger-file: %s", p)
+
+  http.ServeFile(w, r, p)
+}
+
+func serveSwaggerUI(mux *http.ServeMux) {
+  fileServer := http.FileServer(&assetfs.AssetFS{
+	  Asset:    swagger.Asset,
+	  AssetDir: swagger.AssetDir,
+	  Prefix:   "swagger-ui",
+  })
+  prefix := "/swagger-ui/"
+  mux.Handle(prefix, http.StripPrefix(prefix, fileServer))
 }
